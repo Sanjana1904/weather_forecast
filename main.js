31c71c855949e3f71118a671972e363645af9ff0
Sanjana1904 <87716305+Sanjana1904@users.noreply.github.com>
2023-07-17 09:19:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var locationInput = document.getElementById('location-input');
+var weatherInfoDiv = document.getElementById('weather-info');
+var button = document.querySelector('.submit');
+
+button.addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent form submission
+  
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + locationInput.value + '&appid=f4a0b5f21aac35f00417836bc5a1c59c')
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('City not found');
+      }
+    })
+    .then(data => {
+      var tempValue = data['main']['temp'];
+      var humidityValue = data['main']['humidity'];
+      var descValue = data['weather'][0]['description']; 
+      
+      weatherInfoDiv.innerHTML = "<p>Temp - " + tempValue + "&deg;C</p>";
+      weatherInfoDiv.innerHTML += "<p>Desc - " + descValue + "</p>";
+      weatherInfoDiv.innerHTML += "<p>Humidity - " + humidityValue + "</p>";
+
+      locationInput.value = '';
+    })
+    .catch(error => {
+      weatherInfoDiv.innerHTML = "<p>" + error.message + "</p>";
+    });
+});
